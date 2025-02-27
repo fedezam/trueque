8c8706165bfbb3c67176d7e73a97ea507afdc080
fedezam <samprasfed@gmail.com>
2025-02-27 18:31:38 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/11.3.1/firebase
 
 // Configuración de Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyAgIPuOIfvYp191JZI9cKLRkKXfGwdaCxM",
-    authDomain: "trueque-28b33.firebaseapp.com",
-    projectId: "trueque-28b33",
-    storageBucket: "trueque-28b33.appspot.com",  // 🔹 Corregido
-    messagingSenderId: "6430433157",
-    appId: "1:6430433157:web:1e6cf47ee1ed80b127eeec",
-    measurementId: "G-JMDRX032BS"
+  apiKey: "AIzaSyAgIPuOIfvYp191JZI9cKLRkKXfGwdaCxM",
+  authDomain: "trueque-28b33.firebaseapp.com",
+  projectId: "trueque-28b33",
+  storageBucket: "trueque-28b33.firebasestorage.app",
+  messagingSenderId: "6430433157",
+  appId: "1:6430433157:web:1e6cf47ee1ed80b127eeec",
+  measurementId: "G-JMDRX032BS"
 };
 
 // Inicializa Firebase
