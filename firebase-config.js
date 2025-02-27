6f500888a2b7f6a4a67551e2930ea06f368cf569
fedezam <samprasfed@gmail.com>
2025-02-27 18:36:02 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAgIPuOIfvYp191JZI9cKLRkKXfGwdaCxM",
   authDomain: "trueque-28b33.firebaseapp.com",
   projectId: "trueque-28b33",
-  storageBucket: "trueque-28b33.firebasestorage.app",
+  storageBucket: "trueque-28b33.appspot.com",  // 🔹 CORREGIDO 🔹
   messagingSenderId: "6430433157",
   appId: "1:6430433157:web:1e6cf47ee1ed80b127eeec",
   measurementId: "G-JMDRX032BS"
@@ -21,4 +21,3 @@ const googleProvider = new GoogleAuthProvider();
 
 // Exporta las instancias para que otros archivos las usen
 export { app, auth, db, googleProvider };
-
