ea50d57f0e74bd4a4bac2f7eee9d5a9fa1339e9d
fedezam <samprasfed@gmail.com>
2025-02-27 17:49:09 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,16 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
 
 // Configuración de Firebase
 const firebaseConfig = {
-      apiKey: "AIzaSyAgIPuOIfvYp191JZI9cKLRkKXfGwdaCxM",
-      authDomain: "trueque-28b33.firebaseapp.com",
-      projectId: "trueque-28b33",
-      storageBucket: "trueque-28b33.firebasestorage.app",
-      messagingSenderId: "6430433157",
-      appId: "1:6430433157:web:1e6cf47ee1ed80b127eeec",
-      measurementId: "G-JMDRX032BS"
+    apiKey: "AIzaSyAgIPuOIfvYp191JZI9cKLRkKXfGwdaCxM",
+    authDomain: "trueque-28b33.firebaseapp.com",
+    projectId: "trueque-28b33",
+    storageBucket: "trueque-28b33.appspot.com",  // 🔹 Corregido
+    messagingSenderId: "6430433157",
+    appId: "1:6430433157:web:1e6cf47ee1ed80b127eeec",
+    measurementId: "G-JMDRX032BS"
 };
 
 // Inicializa Firebase
@@ -20,6 +19,6 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
 
-// Exporta las instancias para usarlas en otros archivos
+// Exporta las instancias para que otros archivos las usen
 export { app, auth, db, googleProvider };
 
