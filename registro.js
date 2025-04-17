9da42bf6013d96f05fc1b9028d4aff39c13631e5
fedezam <samprasfed@gmail.com>
2025-04-17 10:47:43 -0300

    Update registro.js

--- registro.js
+++ registro.js
@@ -69,7 +69,7 @@ form.addEventListener('submit', async (e) => {
     });
 
     alert('Registro exitoso.');
-    window.location.href = 'inicio.html';
+    window.location.href = 'completar-perfil.html';
 
   } catch (error) {
     if (error.code === 'auth/email-already-in-use') {
